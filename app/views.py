86ad5251023f3279c2fe705d70b4bb15700d3f0d
Fiifi Krampah <fiifi.krampah@gmail.com>
2021-08-14 21:59:16 -0500

    Add routes for error handling

--- app/views.py
+++ app/views.py
@@ -173,6 +173,23 @@ def edit_result():
                 ), 'error')
         return render_template('edit_or_delete.html', form1=form1, device=device, choice='edit')
 
+# error routes
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html', pagetitle="404 Error - Page Not Found", pageheading="Page not found (Error 404)", error=e), 404
+
+
+@app.errorhandler(405)
+def form_not_posted(e):
+    return render_template('error.html', pagetitle="405 Error - Form Not Submitted", pageheading="The form was not submitted (Error 405)", error=e), 405
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('error.html', pagetitle="500 Error - Internal Server Error", pageheading="Internal server error (500)", error=e), 500
+
 
 # Run the app
 if __name__ == '__main__':
