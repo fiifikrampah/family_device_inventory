367a2f6d0fc1dcb125578130a54f69344ef35cf6
Fiifi Krampah <fiifi.krampah@gmail.com>
2022-07-03 17:05:23 -0500

    Change app port from 5000 to 5050
    The default port, 5000, was frequently in conflict with other apps;
    as a result, the port was changed to 5050 to guarantee uniqueness.

--- manage.py
+++ manage.py
@@ -22,7 +22,7 @@ def runserver():
     """
     runserver function utilized for development config
     """
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5050)
 
 
 @manager.command
