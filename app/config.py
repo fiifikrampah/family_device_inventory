ea0f2ff448e158deacc838b3205952c466062be9
Fiifi Krampah <fiifi.krampah@gmail.com>
2022-07-03 16:35:53 -0500

    Reformat py scripts and apply other formatting
    Python scripts have been reformatted with Black formatter.
    Also, missing newline at the EOF have been added to all files

--- app/config.py
+++ app/config.py
@@ -10,12 +10,13 @@ class Config:
     """
     Base Configuration
     """
-    USER = os.environ.get('POSTGRES_USER')
-    PASSWORD = os.environ.get('POSTGRES_PASSWORD')
-    HOST = os.environ.get('POSTGRES_HOST')
-    DATABASE = os.environ.get('POSTGRES_DB')
-    PORT = os.environ.get('POSTGRES_PORT')
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    USER = os.environ.get("POSTGRES_USER")
+    PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+    HOST = os.environ.get("POSTGRES_HOST")
+    DATABASE = os.environ.get("POSTGRES_DB")
+    PORT = os.environ.get("POSTGRES_PORT")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
     DATABASE_URL = f"postgres+psycopg2://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
--- app/views/views.py
+++ app/views/views.py
@@ -4,7 +4,15 @@ This file contains the various routes for the flask app.
 from flask import abort, render_template, request, flash, redirect, url_for, Blueprint
 
 from app.models import db, Users, Devices
-from app.core import generate_id, add_instance, delete_instance, edit_instance, validate_login, WrongSignIn, hash_password
+from app.core import (
+    generate_id,
+    add_instance,
+    delete_instance,
+    edit_instance,
+    validate_login,
+    WrongSignIn,
+    hash_password,
+)
 from sqlalchemy.exc import IntegrityError
 from .forms import UserLogIn, AddUser, AddDevice, DeleteForm
 
@@ -14,7 +22,7 @@ flask_app = Blueprint("flask_app", __name__)  # initialize blueprint
 logged_in = False
 
 
-@flask_app.route('/login', methods=['GET', 'POST'])
+@flask_app.route("/login", methods=["GET", "POST"])
 def login():
     """
     login route
@@ -23,25 +31,25 @@ def login():
     try:
         if form3.validate_on_submit():
             if form3.sign_in.data:
-                username = request.form.get('username')
-                password = hash_password(request.form.get('password'))
+                username = request.form.get("username")
+                password = hash_password(request.form.get("password"))
                 result = validate_login(username, password)
                 if result is True:
                     global logged_in
                     logged_in = True
-                    return redirect(url_for('flask_app.index'))
+                    return redirect(url_for("flask_app.index"))
                 else:
                     raise WrongSignIn
             if form3.sign_up.data:
-                return redirect(url_for('flask_app.signup'))
+                return redirect(url_for("flask_app.signup"))
     except AttributeError:
-        flash('Please enter both your username and password!')
+        flash("Please enter both your username and password!")
     except WrongSignIn:
-        flash('Your username or password is incorrect!')
-    return render_template('login.html', form3=form3)
+        flash("Your username or password is incorrect!")
+    return render_template("login.html", form3=form3)
 
 
-@flask_app.route('/signup', methods=['GET', 'POST'])
+@flask_app.route("/signup", methods=["GET", "POST"])
 def signup():
     """
     signup route
@@ -50,176 +58,182 @@ def signup():
     if form2.validate_on_submit():
         try:
             # add the new user to the database
-            add_instance(Users,
-                         uid=generate_id(),
-                         username=request.form.get('username'),
-                         password=hash_password(request.form.get('password')),
-                         usertype="FamilyMember",
-                         first_name=request.form.get('fname'),
-                         last_name=request.form.get('lname'),
-                         )
-            return redirect(url_for('flask_app.login'))
+            add_instance(
+                Users,
+                uid=generate_id(),
+                username=request.form.get("username"),
+                password=hash_password(request.form.get("password")),
+                usertype="FamilyMember",
+                first_name=request.form.get("fname"),
+                last_name=request.form.get("lname"),
+            )
+            return redirect(url_for("flask_app.login"))
         except IntegrityError:
             flash("Sorry that username is already taken!")
-            return render_template('signup.html', form2=form2)
+            return render_template("signup.html", form2=form2)
     else:
         # show validaton errors
         for field, errors in form2.errors.items():
             for error in errors:
-                flash("Error in {}: {}".format(
-                    getattr(form2, field).label.text,
-                    error
-                ), 'error')
-        return render_template('signup.html', form2=form2)
+                flash(
+                    "Error in {}: {}".format(getattr(form2, field).label.text, error),
+                    "error",
+                )
+        return render_template("signup.html", form2=form2)
 
 
-@flask_app.route('/')
+@flask_app.route("/")
 def index():
     """
     index route
     """
     if logged_in is True:
         # get a list of unique values in the category column
-        device_categories = Devices.query.with_entities(
-            Devices.category).distinct()
-        return render_template('index.html',
-                               device_categories=device_categories)
+        device_categories = Devices.query.with_entities(Devices.category).distinct()
+        return render_template("index.html", device_categories=device_categories)
     else:
-        return redirect(url_for('flask_app.login'))
+        return redirect(url_for("flask_app.login"))
 
 
-@flask_app.route('/inventory/<category>')
+@flask_app.route("/inventory/<category>")
 def inventory(category):
     """
     inventory route
     """
-    tech = Devices.query.filter_by(
-        category=category).order_by(Devices.name).all()
-    return render_template('device_list.html', tech=tech, category=category)
+    tech = Devices.query.filter_by(category=category).order_by(Devices.name).all()
+    return render_template("device_list.html", tech=tech, category=category)
+
 
 # add a new device to the database
 
 
-@flask_app.route('/add_device', methods=['GET', 'POST'])
+@flask_app.route("/add_device", methods=["GET", "POST"])
 def add_device():
     """
     add device route
     """
     form1 = AddDevice()
     if form1.validate_on_submit():
-        add_instance(Devices,
-                     id=generate_id(),
-                     name=request.form['device_name'],
-                     device_type=request.form['device_type'],
-                     serial=request.form['device_serial'],
-                     model=request.form['device_model'],
-                     mac_address=request.form['device_mac'],
-                     status=request.form['status'],
-                     purchase_date=request.form['purchase_date'],
-                     owner_username=request.form['owner'],
-                     category=request.form['category'],
-                     notes=request.form['notes']
-                     )
+        add_instance(
+            Devices,
+            id=generate_id(),
+            name=request.form["device_name"],
+            device_type=request.form["device_type"],
+            serial=request.form["device_serial"],
+            model=request.form["device_model"],
+            mac_address=request.form["device_mac"],
+            status=request.form["status"],
+            purchase_date=request.form["purchase_date"],
+            owner_username=request.form["owner"],
+            category=request.form["category"],
+            notes=request.form["notes"],
+        )
         # create a message to send to the template
         message = f"The data for device {request.form['device_name']} \
                     has been submitted."
-        return render_template('add_device.html', message=message)
+        return render_template("add_device.html", message=message)
     else:
         # show validaton errors
         for field, errors in form1.errors.items():
             for error in errors:
-                flash("Error in {}: {}".format(
-                    getattr(form1, field).label.text,
-                    error
-                ), 'error')
-        return render_template('add_device.html', form1=form1)
+                flash(
+                    "Error in {}: {}".format(getattr(form1, field).label.text, error),
+                    "error",
+                )
+        return render_template("add_device.html", form1=form1)
+
 
 # select a device to edit or delete
 
 
-@flask_app.route('/select_device/<letters>')
+@flask_app.route("/select_device/<letters>")
 def select_device(letters):
     """
     select device route
     """
     # Alphabetical sort of devices by name, chunked by letters between _ and _
     a, b = list(letters)
-    tech = Devices.query.filter(
-        Devices.name.between(a, b)).order_by(Devices.name).all()
-    return render_template('select_device.html', tech=tech)
+    tech = Devices.query.filter(Devices.name.between(a, b)).order_by(Devices.name).all()
+    return render_template("select_device.html", tech=tech)
 
 
 # edit or delete a device
-@flask_app.route('/edit_or_delete', methods=['POST'])
+@flask_app.route("/edit_or_delete", methods=["POST"])
 def edit_or_delete():
     """
     edit or delete route
     """
-    id = request.form['id']
-    choice = request.form['choice']
+    id = request.form["id"]
+    choice = request.form["choice"]
     device = Devices.query.filter(Devices.id == id).first()
     form1 = AddDevice()
     form2 = DeleteForm()
-    return render_template('edit_or_delete.html', device=device,
-                           form1=form1, form2=form2, choice=choice)
+    return render_template(
+        "edit_or_delete.html", device=device, form1=form1, form2=form2, choice=choice
+    )
 
 
 # result of delete - this function deletes the record
-@flask_app.route('/delete_result', methods=['POST'])
+@flask_app.route("/delete_result", methods=["POST"])
 def delete_result():
     """
     delete route
     """
-    id = request.form['id_field']
-    purpose = request.form['purpose']
-    if purpose == 'delete':
+    id = request.form["id_field"]
+    purpose = request.form["purpose"]
+    if purpose == "delete":
         deleted_device = delete_instance(Devices, id)
         db.session.commit()
         message = f"The device {deleted_device} has been deleted \
                     from the database."
-        return render_template('result.html', message=message)
+        return render_template("result.html", message=message)
     else:
         # this calls an error handler
         abort(405)
 
+
 # result of edit - this function updates the record
 
 
-@flask_app.route('/edit_result', methods=['POST'])
+@flask_app.route("/edit_result", methods=["POST"])
 def edit_result():
     """
     edit result route
     """
-    id = request.form['id_field']
-    device = edit_instance(Devices, id,
-                           name=request.form['device_name'],
-                           device_type=request.form['device_type'],
-                           serial=request.form['device_serial'],
-                           model=request.form['device_model'],
-                           mac_address=request.form['device_mac'],
-                           status=request.form['status'],
-                           purchase_date=request.form['purchase_date'],
-                           owner_username=request.form['owner'],
-                           category=request.form['category'],
-                           notes=request.form['notes']
-                           )
+    id = request.form["id_field"]
+    device = edit_instance(
+        Devices,
+        id,
+        name=request.form["device_name"],
+        device_type=request.form["device_type"],
+        serial=request.form["device_serial"],
+        model=request.form["device_model"],
+        mac_address=request.form["device_mac"],
+        status=request.form["status"],
+        purchase_date=request.form["purchase_date"],
+        owner_username=request.form["owner"],
+        category=request.form["category"],
+        notes=request.form["notes"],
+    )
     form1 = AddDevice()
     if form1.validate_on_submit():
         # create a message to send to the template
         message = f"The data for device {device[1]} has been updated."
-        return render_template('result.html', message=message)
+        return render_template("result.html", message=message)
     else:
         # show validaton errors
         device[0] = id
 
         for field, errors in form1.errors.items():
             for error in errors:
-                flash("Error in {}: {}".format(
-                    getattr(form1, field).label.text,
-                    error
-                ), 'error')
-        return render_template('edit_or_delete.html',
-                               form1=form1, device=device, choice='edit')
+                flash(
+                    "Error in {}: {}".format(getattr(form1, field).label.text, error),
+                    "error",
+                )
+        return render_template(
+            "edit_or_delete.html", form1=form1, device=device, choice="edit"
+        )
+
 
 # error routes
 
@@ -229,10 +243,15 @@ def page_not_found(e):
     """
     page not found error
     """
-    return render_template('error.html',
-                           pagetitle="404 Error - Page Not Found",
-                           pageheading="Page not found (Error 404)",
-                           error=e), 404
+    return (
+        render_template(
+            "error.html",
+            pagetitle="404 Error - Page Not Found",
+            pageheading="Page not found (Error 404)",
+            error=e,
+        ),
+        404,
+    )
 
 
 @flask_app.errorhandler(405)
@@ -240,10 +259,15 @@ def form_not_posted(e):
     """
     form not posted error
     """
-    return render_template('error.html',
-                           pagetitle="405 Error - Form Not Submitted",
-                           pageheading="Form was not submitted (Error 405)",
-                           error=e), 405
+    return (
+        render_template(
+            "error.html",
+            pagetitle="405 Error - Form Not Submitted",
+            pageheading="Form was not submitted (Error 405)",
+            error=e,
+        ),
+        405,
+    )
 
 
 @flask_app.errorhandler(500)
@@ -251,7 +275,12 @@ def internal_server_error(e):
     """
     internal server error
     """
-    return render_template('error.html',
-                           pagetitle="500 Error - Internal Server Error",
-                           pageheading="Internal server error (500)",
-                           error=e), 500
+    return (
+        render_template(
+            "error.html",
+            pagetitle="500 Error - Internal Server Error",
+            pageheading="Internal server error (500)",
+            error=e,
+        ),
+        500,
+    )
